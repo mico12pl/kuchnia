41fe790f335f1ed22f174487db16baae6563eb08
mico <58231490+mico12pl@users.noreply.github.com>
2026-09-24 13:10:46 +0200

    Set valid CLIENT_ID for KUCHNIA_CONFIG
    Updated CLIENT_ID in KUCHNIA_CONFIG with a valid OAuth client ID.

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 window.KUCHNIA_CONFIG = {
   // Wymagane: identyfikator klienta OAuth (typ „Web application”) z Google Cloud Console,
   // np. '1234567890-abc123.apps.googleusercontent.com'
-  CLIENT_ID: '',
+  CLIENT_ID: '310224762542-1qf2774qo7o4r6c6vrpc915hb1v604qt.apps.googleusercontent.com',
 
   // Opcjonalne: ID folderu „Kuchnia” na Dysku (fragment adresu po /folders/).
   // Ustaw, jeśli ktoś ma kilka folderów o tej nazwie.
